dd4b3c636a2a661d68b431c7c8e4293e64b2e02b
Ashwin-3cS <flashashwin2004@gmail.com>
2023-12-10 18:54:21 +0530

    just learned something today so just added to repository

---
+++ first.js
@@ -0,0 +1,157 @@
+// let day='saturday'
+// switch(day){
+//     case 'monday':
+//         console.log('7-am')
+//         break;
+//     case 'tuesday':
+//     case "wednesday":
+//         console.log('4-am');
+//         break;
+//     case 'thursday':
+//         console.log('2-am')
+//         break;
+//     case 'friday':
+//         console.log('sleep well')
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('upto 12 sleep')
+//         break;
+//     default:
+//         console.log('its a holiday')
+// }
+
+
+
+// let num=2
+// let num2=8
+// let result=num+num2
+// console.log(`the addition of ${num} and ${num2} is ${result}`)
+
+
+// let i=20;
+// while(i>=10)
+// {
+//     console.log('hi',i);
+//     i--;
+// }
+
+
+// let i=20
+// for(;i>=10;)
+//     {
+//         console.log('bro',i)
+//         i--;
+//     }
+
+
+
+// for(i=1;i<=100;i++)
+// {
+//     if (i%2===1)
+//     console.log(i)
+// }
+
+
+// let ash={
+//     name:'ashwin',
+//     lap:'hp_laptop'}
+//     console.log(ash['lap'])
+
+
+// let alien={
+//          name:'ashwin',
+//          tech:'js',
+//          laptop:{
+//             cpu:'i7',
+//             ram:4,
+//             brand:'asus'
+//          }
+//         }
+//         for(let key in alien){
+//             console.log(key,alien.laptop)}
+      
+
+
+// let func=()=>
+// {
+//     return 'hi'
+// }
+    
+
+// for(i=1;i<5;i++)
+// {
+// console.log(func())
+// }
+
+
+
+// function boy(hair)
+// {
+//     return `${user} is curly`
+
+// }
+
+// let user='ashwin'
+// let str= boy()
+// console.log(str)
+
+
+// let add= (num1,num2)=>
+// {
+//     return num1+num2
+// }
+// let result=add(3,7)
+// console.log(result)
+
+// let add=(num1,num2)=>num1+num2
+// let result= add(8,9)
+// console.log(result)
+
+// data={
+//     ash:'bad',
+//     hair:'curly',
+//     func:function gender(){
+//         console.log('male')
+//     }
+   
+// }
+// data.func()
+// console.log(data.ash)
+
+
+// let ash=[1,'bad',function teller(){
+
+//     console.log('welll welll welll')
+
+// }]
+// ash[2]();
+// // console.log(ash[0]);
+// ash[3]=123
+// console.log(ash)
+
+
+// let a=[5,7,8,9]
+// console.log(a.splice(2,1,11,17))
+// console.log(a)
+// // console.log(a.push(4,8))
+// // console.log(a)
+
+// let a=[5,8,9,7]
+// let[b,c,d,e]=a
+// console.log(d)
+
+
+
+//swapping of two numbers using array destructuring
+// let a=1;
+// let b=8;
+// [a,b]=[b,a]
+// console.log(b);
+
+
+
+// let nums=[1,2,5,3,4]
+// nums.forEach((n)=>console.log(n+1))
+
+
---
+++ practise.js
@@ -0,0 +1,20 @@
+// function shortlife(age){
+//   expectedage=90;
+//   currentAge=expectedage-age;
+//   var days=365;
+//   var weeks=52;
+//   var months=12;
+//   console.log("You have "+" "+(currentAge*days)+"days"+" "+(currentAge*weeks)+"weeks"+" "+(currentAge*months)+"months")
+
+// }
+// shortlife(19);
+
+
+// function bmiCalculator(weight,height){
+//   var ans=(weight/(height*height));
+//   return Math.floor(ans);
+// }
+// var bmi=bmiCalculator(65,1.8);
+// console.log("Body mass index of this person is",bmi);
+
+
