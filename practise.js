d107a290fbdaf96fdd8730e663f19c4d9721c7cb
Ashwin-3cS <flashashwin2004@gmail.com>
2023-12-13 15:42:04 +0530

    while loop code addded

--- practise.js
+++ practise.js
@@ -76,3 +76,14 @@
 // randomName()
 
 
+function beer(){
+  bottles=100
+  while(bottles>=1){
+    console.log(bottles,"bottles of beer on the wall ",bottles,"bottles of beer. Take 1 down, pass it around ,",bottles,"bottles of beer on the wall.")
+    bottles--;
+  }
+}
+beer()
+
+
+
